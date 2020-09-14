c4e1e0368593086d1f3ec8fbbd10bf65d3a1d23e
Shekhar <meshekharbehera613@gmail.com>
2020-09-15 02:05:22 +0530

    X00P: Drop Telephony jars Signed-off-by: Flamefusion <meshekharbehera613@gmail.com>

--- device.mk
+++ device.mk
@@ -388,13 +388,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_HOST_PACKAGES += \
     signapk
 
-# Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
